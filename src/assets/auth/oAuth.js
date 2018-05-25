d56b1910418a954a73592b03a5b136b6a564ed42
Pat <pat.emery21@gmail.com>
2018-05-25 10:36:19 -0400

    Deploy fix

---
+++ src/assets/auth/oAuth.js
@@ -0,0 +1,9 @@
+export const auth = {
+	client_id: 'JYyGj1fuRxnM1Q',
+	client_secret: 'lIEzNCTL3lHRe_Od8C0AYsNKvmI',
+	refresh_token: '40158280-aQxOrTrnKQqPUbz9Sk1sLxDOCIg',
+	user_agent: 'react-reddit-develop',
+	username: 'selfoscillation',
+	password: 'password',
+	redirect_uri: 'https://not-an-aardvark.github.io/reddit-oauth-helper/'
+};
